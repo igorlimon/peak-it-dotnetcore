0e53eb953e43a4f93ed5186a0548e2c0518a22f5
limon <igorz.jigan@gmail.com>
2018-12-09 10:03:09 +0200

    Module1

---
+++ PeakItConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeakItConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PeakItMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PeakItMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
